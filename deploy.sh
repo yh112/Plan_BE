5b991a6c0d83a1d5cfd002731c38aadcd4a5b3e0
yh112 <msh7377@gmail.com>
2025-03-20 17:06:48 +0900

    Fix: pm2 관련 코드 추가

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,19 @@ git pull origin main
 
 npm install
 
-pm2 restart all
+# 애플리케이션 상태 확인
+pm2 list | grep 'app.js' > /dev/null
+
+# 애플리케이션이 실행 중인지 확인하고, 실행 중이면 재시작, 없으면 새로 시작
+if [ $? -eq 0 ]; then
+  # 이미 실행 중이면 애플리케이션 재시작
+  echo "Application is already running. Restarting..."
+  pm2 restart app.js || exit
+else
+  # 실행 중이지 않으면 새로 시작
+  echo "Application is not running. Starting..."
+  pm2 start app.js || exit
+fi
+
+# 애플리케이션 상태 확인
+pm2 list
