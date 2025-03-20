dfa46fa882b217d9e45091602c6456490cb5e313
yh112 <msh7377@gmail.com>
2025-03-20 17:12:44 +0900

    Fix: git pull 로그 추가

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,9 @@ ssh-add ~/.ssh/id_rsa
 # Git 리모트 URL을 SSH로 변경
 git remote set-url origin git@github.com:yihyun/Plan_BE.git
 
+git pull origin main
+git log -1  # 가장 최신 커밋을 확인
+
 npm install
 
 # 애플리케이션 상태 확인
