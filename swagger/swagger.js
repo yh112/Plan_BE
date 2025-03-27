6972e0f8cab7500e54b40dc663497cd0acd2be48
yh112 <msh7377@gmail.com>
2025-03-27 13:10:22 +0900

    Fix: 스웨거 yaml 연결

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -1,31 +1,45 @@
+// const swaggerUi = require("swagger-ui-express");
+// const swaggerJsdoc = require("swagger-jsdoc");
+// const path = require("path");
+
+// const options = {
+//   definition: {
+//     openapi: "3.0.0",
+//     info: {
+//       title: "Weekly Plan API",
+//       description: "Weekly Plan Web App RESTful API Documentation",
+//       contact: {
+//         name: "Yihyun",
+//         email: "msh7377@gmail.com",
+//       },
+//       version: "1.0.0",
+//     },
+//     servers: [
+//       {
+//         url: "http://localhost:4000",
+//         description: "Local Development",
+//       },
+//     ],
+//   },
+//   apis: [path.join(__dirname, "../routes/*.js")],
+// };
+// const specs = swaggerJsdoc(options);
+
+// module.exports = {
+//   swaggerUi,
+//   specs,
+// };
+
 const swaggerUi = require("swagger-ui-express");
-const swaggerJsdoc = require("swagger-jsdoc");
+const fs = require("fs");
+const yaml = require("js-yaml");
 const path = require("path");
 
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Weekly Plan API",
-      description: "Weekly Plan Web App RESTful API Documentation",
-      contact: {
-        name: "Yihyun",
-        email: "msh7377@gmail.com",
-      },
-      version: "1.0.0",
-    },
-    servers: [
-      {
-        url: "http://localhost:4000",
-        description: "Local Development",
-      },
-    ],
-  },
-  apis: [path.join(__dirname, "../routes/*.js")],
-};
-const specs = swaggerJsdoc(options);
+const swaggerDocument = yaml.load(
+  fs.readFileSync(path.join(__dirname, "../swagger.yaml"), "utf8")
+);
 
 module.exports = {
   swaggerUi,
-  specs,
+  specs: swaggerDocument,
 };
